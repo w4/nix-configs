14972dce270d0a4a1a23da02373b0c9389ec9610
Jordan Johnson-Doyle <jordan@doyle.la>
2019-02-04 21:29:56 +0000

    Switch from caddy to haproxy, add forwarding to plex

--- proxy.nix
+++ proxy.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
@@ -15,27 +19,99 @@
   # Set your time zone.
   time.timeZone = "Europe/London";
 
-  services.caddy = {
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
+
+  services.haproxy = {
     enable = true;
     config = ''
-      shed.doyle.la {
-        gzip
-        log syslog
+      defaults
+        log     global
+        mode    http
+        option  httplog
+        option  dontlognull
+        option redispatch
+        option http-use-htx
+        retries 3
+        maxconn 2048
+        timeout connect 5000
+        timeout client 50000
+        timeout server 50000
 
-        proxy / http://10.0.0.14 {
-          transparent
-        }
-      }
+      frontend fe-stats
+        bind *:8080
+        stats enable
+        stats uri /
+        stats realm Haproxy\ Statistics
+
+      frontend fe-http
+        bind :::80 v4v6
+        bind :::443 v4v6 ssl crt /var/lib/acme/shed.doyle.la/full.pem crt /var/lib/acme/plex.doyle.la/full.pem alpn h2,http/1.1
+
+        option forwardfor
+        http-request add-header X-CLIENT-IP %[src]
+        http-request set-header X-Forwarded-Host %[req.hdr(Host)]
+        http-request set-header X-Forwarded-Proto https
+
+        redirect scheme https code 301 if !{ ssl_fc }
+
+        acl letsencrypt-acl path_beg /.well-known/acme-challenge/
+        use_backend be-letsencrypt if letsencrypt-acl
+
+        acl cloud-acl hdr(host) -i shed.doyle.la
+        use_backend be-cloud if cloud-acl
+
+        acl plex-acl hdr(host) -i plex.doyle.la
+        use_backend be-plex if plex-acl
+
+        http-response add-header X-App-Server %b/%s
+
+      backend be-cloud
+        server cloud 10.0.0.14:80
+
+      backend be-plex
+        server plex 10.0.0.20:32400
+
+      backend be-letsencrypt
+        server letsencrypt 127.0.0.1:8888
     '';
-    email = "jordan@doyle.la";
-    agree = true;
+  };
+
+  systemd.services.haproxy.after = [ "network.target" "acme-selfsigned-certificates.target" ];
+  systemd.services.haproxy.wants = [ "acme-selfsigned-certificates.target" "acme-certificates.target" ];
+
+  security.acme.certs = {
+    "plex.doyle.la" = {
+      webroot = "/var/www/html";
+      email = "jordan@doyle.la";
+    };
+    "shed.doyle.la" = {
+      webroot = "/var/www/html";
+      email = "jordan@doyle.la";
+    };
+  };
+  # security.acme.production = false;
+
+  services.nginx = {
+    enable = true;
+    recommendedOptimisation = true;
+
+    virtualHosts = {
+      localhost = {
+        listen = [{ addr = "127.0.0.1"; port = 8888; }];
+        root = "/var/www/html";
+      };
+    };
   };
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 80 443 2015 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 8080 ];
   networking.firewall.allowedUDPPorts = [ 443 ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
